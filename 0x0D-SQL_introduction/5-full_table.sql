ab8aad2822048b6aa71ef4425c92d101cdbec864
EL YAZIDI <abdo.elyazidi2018@gmail.com>
2023-11-14 10:46:54 -0800

    Task 6

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Full table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW COLUMNS FROM hbtn_0c_0.first_table;
