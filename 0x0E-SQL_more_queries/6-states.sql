8ad3d04844259f9439fc28cd19f1cce8f825cff4
Elyazidi <abdo.elyazidi2018@gmail.com>
2023-11-16 00:22:50 +0100

    Task 7

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creating a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT NOT NULL AUTO INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
