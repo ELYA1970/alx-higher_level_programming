46161a8aa8e9f1191d17798cc80e6e869abc7857
Elyazidi <abdo.elyazidi2018@gmail.com>
2023-11-15 23:38:36 +0100

    Task 3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- Creating User and database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_0'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_0'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_d.* TO 'user_0d_0'@'localhost';
